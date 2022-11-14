84a349596138393a20feb75e23506fdfee679b28
ronfeldman <ronfeldman2003@gmail.com>
2022-11-14 22:41:03 +0200

    COMPLETE 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function (tempInF) {
+  let tempInC = (tempInF - 32) * (5 / 9);
+  return Math.round((tempInC + Number.EPSILON) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (tempInC) {
+  let tempInF = (tempInC*(9/5)+32);
+  return Math.round((tempInF + Number.EPSILON) * 10) / 10;
 };
 
 // Do not edit below this line
