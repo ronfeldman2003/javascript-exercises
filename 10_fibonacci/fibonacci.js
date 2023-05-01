d117f8659dc7e03a26f764849200a7f388e3f03a
ronfeldman <ronfeldman2003@gmail.com>
2023-05-01 23:04:22 +0300

    10 - DONE

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if(index<0){
+        return "OOPS";
+    }
+    if(index<=2){
+        return 1;
+    }else{return (fibonacci(index-1)+fibonacci(index-2));};
 };
 
 // Do not edit below this line
