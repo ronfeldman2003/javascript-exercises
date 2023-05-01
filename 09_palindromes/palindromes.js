97879564c90e081333cbc4ea3c47450798eec696
ronfeldman <ronfeldman2003@gmail.com>
2023-05-01 22:37:02 +0300

    09 - DONE

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (text) {  
 
+    let palimdrom = text.replaceAll('!','').replaceAll('.','').replaceAll(',','').replaceAll(' ','').trim().toLowerCase();
+    let textArray = palimdrom.split("").reverse().join("");
+    return palimdrom==textArray;
 };
 
 // Do not edit below this line
