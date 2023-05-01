e7440e3c95bca188ce320875c4ff7af824435158
ronfeldman <ronfeldman2003@gmail.com>
2023-05-01 22:15:21 +0300

    08 - DONE

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
+const add = function(a,b) {
+	return a + b;
+};
+
+const subtract = function(a,b) {
+	return a-b;
+};
+
+const sum = function(arr) {
+	 return arr.reduce((a,b)=>a+b,0);
+};
+
+const multiply = function(arr) {
+  return arr.reduce((a,b)=>a*b);
+};
+
+const power = function(a,b) {
+  return Math.pow(a,b);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num==0){return 1};
+  if(num>0){return (num*factorial(num-1))};
 };
 
 // Do not edit below this line
