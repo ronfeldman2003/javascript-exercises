c06504dd0d8c1e0e1536a9171b9d8e50dc125280
ronfeldman <ronfeldman2003@gmail.com>
2023-05-01 23:28:49 +0300

    12 - DONE

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest,current)=>{
+        let current_age
+        let oldest_age
+        let today = new Date().getFullYear();
+        if('yearOfDeath' in current){
+            current_age =current.yearOfDeath-current.yearOfBirth;
+        }else{
+            current_age = today-current.yearOfBirth;
+        }
+        if('yearOfDeath' in oldest){
+            oldest_age =oldest.yearOfDeath-oldest.yearOfBirth;}else{
+            oldest_age =today-oldest.yearOfBirth;
+        }
+        if(current_age>oldest_age){
+            return current;
+        }else{
+            return oldest;
+        }
+    });
 };
 
 // Do not edit below this line
